1afedd4f24a003b07c01beec07d1574595b656db
Tom Shull <mail@tomshull.com>
2020-05-19 16:32:19 -0500

    removing code not being used

--- running_scripts/launch_local.sh
+++ running_scripts/launch_local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-./tfb_host_network.bash \
+${FRAMEWORK_DIR}/running_scripts/tfb_host_network.bash \
     --server-host 127.0.0.1 \
     --database-host 127.0.0.1 \
     --client-host 127.0.0.1 \
